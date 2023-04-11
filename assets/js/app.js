b64dc8720dc4ef12bad0d348624a8e3724060d63
DanieleMorello <danielemorello@live.it>
2023-04-11 16:53:40 +0200

    change name of email to emails and change type in array, create emailsGenerated and delete my-data, add a for loop and push data on emails array. in html add a v-for on li

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,28 +1,20 @@
 const { createApp } = Vue
+
 createApp({
   data() {
     return {
-      title: 'Email:',
+      title: 'Emails:',
       url: "https://flynn.boolean.careers/exercises/api/random/mail",
-      my_data: null,
-      email: null,
+      emails: [],
+      emailsGenerated: 10,
     }
   },
   mounted() {
-    // console.log(axios);
-    
-    //console.log(axios.get(this.url)); // Promise
-    // this.url (uri+request body)
-    axios
-    .get(this.url)
-    .then(response => {
-      // prendo l'intero responso (solo i dati) e lo salvo nell'oggetto vue
-      console.log(response); // contiene un oggetto 
-      //console.log(response.data);
-      this.my_data = response.data 
-      // prendo dal responso solo i dati di cui ho bisogno.
-      console.log(response.data.response);
-      this.email= response.data.response
-    })
+    for (let i = 0; i < this.emailsGenerated; i++) {
+      axios.get(this.url)
+        .then(response => {
+          this.emails.push(response.data.response);
+        })
+    }
   },
 }).mount('#app')
