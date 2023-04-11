76d3162ad5fa52b4d48b014124820a1cd59fce94
DanieleMorello <danielemorello@live.it>
2023-04-11 15:40:01 +0200

    Print an eamil

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,6 +3,26 @@ createApp({
   data() {
     return {
       title: 'Email:',
+      url: "https://flynn.boolean.careers/exercises/api/random/mail",
+      my_data: null,
+      email: null,
     }
-  }
+  },
+  mounted() {
+    // console.log(axios);
+    
+    //console.log(axios.get(this.url)); // Promise
+    // this.url (uri+request body)
+    axios
+    .get(this.url)
+    .then(response => {
+      // prendo l'intero responso (solo i dati) e lo salvo nell'oggetto vue
+      console.log(response); // contiene un oggetto 
+      //console.log(response.data);
+      this.my_data = response.data 
+      // prendo dal responso solo i dati di cui ho bisogno.
+      console.log(response.data.response);
+      this.email= response.data.response
+    })
+  },
 }).mount('#app')
